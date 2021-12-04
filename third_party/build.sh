5a9d5ba0da11e446ab09c1a9de85931e6771dada
oneofthezombies <hunhoekim@gmail.com>
2021-12-05 05:58:09 +0900

    removed glew glfw source.

--- third_party/build.sh
+++ third_party/build.sh
@@ -1,10 +1,5 @@
-# build glew macos arm64
-cd source/glew-2.2.0/build
-cmake -DCMAKE_APPLE_SILICON_PROCESSOR=arm64 ./cmake
-make -j4
+# glew macos
+brew install glew
 
-# build glfw macos arm64
-cd source/glfw-3.3.5
-cmake -DCMAKE_APPLE_SILICON_PROCESSOR=arm64 -DBUILD_SHARED_LIBS=ON -Bbuild .
-cd build
-make -j4
+# glfw macos
+brew install glfw
