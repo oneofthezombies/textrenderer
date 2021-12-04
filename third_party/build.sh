b4ef2360d0b297928fd4e4759a0eb5d3713aa758
oneofthezombies <hunhoekim@gmail.com>
2021-12-05 04:13:12 +0900

    modified glfw build command.

--- third_party/build.sh
+++ third_party/build.sh
@@ -1,4 +1,10 @@
 # build glew macos arm64
-cd glew-2.2.0/build
+cd source/glew-2.2.0/build
 cmake -DCMAKE_APPLE_SILICON_PROCESSOR=arm64 ./cmake
+make -j4
+
+# build glfw macos arm64
+cd source/glfw-3.3.5
+cmake -DCMAKE_APPLE_SILICON_PROCESSOR=arm64 -DBUILD_SHARED_LIBS=ON -Bbuild .
+cd build
 make -j4
