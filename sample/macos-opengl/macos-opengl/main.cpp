57f163f59b4cc8b4448f5b268aa64f64f888980d
oneofthezombies <hunhoekim@gmail.com>
2021-12-05 05:33:56 +0900

    added sample macos project.

---
+++ sample/macos-opengl/macos-opengl/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  macos-opengl
+//
+//  Created by hunhoe kim on 2021/12/05.
+//
+
+#include <GL/glew.h>
+#include "ignore_warning/push.h"
+#include "ignore_warning/Wdocumentation.h"
+#include "ignore_warning/Wdocumentation-deprecated-sync.h"
+#include <GLFW/glfw3.h>
+#include "ignore_warning/pop.h"
+
+int main(int argc, const char* argv[]) {
+    return 0;
+}
