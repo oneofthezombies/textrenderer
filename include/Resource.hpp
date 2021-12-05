5ac5054121dec290e5a84ad0751b71c66427dbf7
oneofthezombies <hunhoekim@gmail.com>
2021-12-06 04:49:40 +0900

    added resource.

---
+++ include/Resource.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <functional>
+
+template<
+    typename Signature,
+    typename ValueType,
+    typename ResourceTraits,
+    typename ...AcquirerArguments>
+struct Resource {
+    using self_type = Resource;
+    using signature_type = Signature;
+    using value_type = ValueType;
+    using traits_type = ResourceTraits;
+    
+    Resource(AcquirerArguments&&... acquirerArguments)
+        : value_(traits_type::getAcquirer()(std::forward<AcquirerArguments&&>(acquirerArguments)...)) {
+    }
+    
+    ~Resource() {
+        const value_type undefined = traits_type::getUndefinedValue();
+        if (undefined != value_) {
+            traits_type::getReleaser()(value_);
+            value_ = undefined;
+        }
+    }
+    value_type value_;
+};
+
+template<
+    typename Signature,
+    typename ValueType,
+    typename ...AcquirerArguments>
+struct ResourceTraits {
+    using self_type = ResourceTraits;
+    using signature_type = Signature;
+    using value_type = ValueType;
+    using acquirer_type = std::function<value_type(AcquirerArguments&&...)>;
+    using releaser_type = std::function<void(value_type&)>;
+    
+    static value_type getUndefinedValue() { return value_type(); }
+    static acquirer_type getAcquirer() { return acquirer_type(); }
+    static releaser_type getReleaser() { return releaser_type(); }
+};
